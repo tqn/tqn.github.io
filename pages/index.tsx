fee96aa89cc9e41ba63c9716851b43a247edadf2
Tej Qu Nair <tej.q.nair@gmail.com>
2018-06-30 02:45:44 -0700

    Add (legacy) decorator support

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
 
-import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
 
 import Layout from '../components/Layout';
 
-export default class extends React.Component {
+@withStyles(theme => ({
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+  },
+  root: {
+    flexGrow: 1,
+  },
+}))
+class Index extends React.Component {
   public render() {
     return (
       <Layout>
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
+        <div className={this.props.classes.root}>
+          <Grid container>
+            <Grid item xs={12}>
+              <Paper className={this.props.classes.paper}>hello</Paper>
+            </Grid>
+          </Grid>
+        </div>
       </Layout>
     );
   }
 }
+
+export default Index;
